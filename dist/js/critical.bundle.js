75253a5af3cfb72cb003585d79bedb6ae6058953
Applee_ch <Applee@Applee-2.local>
2016-07-22 11:50:59 -0700

    add sass for set font-size add media query

--- dist/js/critical.bundle.js
+++ dist/js/critical.bundle.js
@@ -1,2 +1,1 @@
-!function(e,n){var t=e.documentElement,i="onorientationchange"in window?"orientationchange":"resize",o=function(){var e=t.clientWidth,n=20*(e/375);n=n>32?32:n,e&&(t.style.fontSize=n+"px")};n.addEventListener(i,o,!1),e.addEventListener("DOMContentLoaded",o,!1)}(document,window);
 
